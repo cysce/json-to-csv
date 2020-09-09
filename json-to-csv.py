1dc3aec84036d0c0edaa860d1da164ac21fb81bc
GermanTorresM <GermanTorresM@users.noreply.github.com>
2020-09-09 13:42:43 -0300

    Update json-to-csv.py
    Initial Commit

--- json-to-csv.py
+++ json-to-csv.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print ("\nUsage: python json-to-csv.py <json_in_file_path> <csv_out_file_path>\n")
 else:
     #Reading arguments
     json_file_path = sys.argv[1]
     csv_file_path = sys.argv[2]
-        
+
     df = pd.read_json(json_file_path)
     #df = pd.read_json(json_file_path, orient='index')
     df.to_csv(csv_file_path, index = None, header=True)
-    
-    print ("File CSV created")
+
+    print ("File CSV created"):
